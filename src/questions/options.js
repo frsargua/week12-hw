9001682235d987ca41149cf065f0817e29d219bb
frsargua <frsargua@gmail.com>
2022-07-01 15:43:55 +0100

    Options for inquirer

---
+++ src/questions/options.js
@@ -0,0 +1,19 @@
+const options = [
+  {
+    name: "mainOptions",
+    message: "OPTIONS:",
+    type: "list",
+    choice: [
+      "View all departments",
+      "View all roles",
+      "View all employees",
+      "Add a department",
+      "Add a role",
+      "Add an employee",
+      "Update Employee role:",
+      "QUIT",
+    ],
+  },
+];
+
+// module.exports = options;
